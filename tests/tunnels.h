db5cbf87bd324a8c42d0e415603ed45a198be6a4
Esteban Egea Lopez <esteban.egea@gmail.com>
2020-02-07 13:34:59 +0100

    Fixed error in tunnels.h

--- tests/tunnels.h
+++ tests/tunnels.h
@@ -12,7 +12,7 @@
 using namespace opal;
 
 //Simulate propagation inside a tunnel. Used to exemplify partial launches with low angular separation (precise angular sampling). To be run with a high number of reflections, such as 20 or 30
-std::unique_ptr<OpalSceneManager> cubeTunnel(std::unique_ptr<OpalSceneManager> sceneManager, float sphereRadius) ;
+std::unique_ptr<OpalSceneManager> cubeTunnel(std::unique_ptr<OpalSceneManager> sceneManager) ;
 void xcut(OpalSceneManager* sceneManager, float width, float height, float y, float distance, float3 polarization, float sphereRadius); 
 void ycut(OpalSceneManager* sceneManager, float width, float height, float x, float distance, float3 polarization, float sphereRadius) ;
 void zrun(OpalSceneManager* sceneManager, float zinit, float deltad, float x, float y, float length, float3 polarization, float sphereRadius) ;
